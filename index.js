58446487abc6caf98f85e5d0ab48f35dd42d0799
Will Yazdani <yazd0006@gmail.com>
2022-03-20 19:49:01 -0500

    add employee started

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
 require('console.table');
+
+const init = () => {
+    inquirer.prompt({
+        name: "selection",
+        type: "list",
+        choices: ["Add an employee", "Add a department", "Add a role", "View all employees", "View all departments", "View all roles"]
+    })
+    .then(data => {
+        if (data.selection === "Add an employee") {
+            enterEmployee()
+        }
+    })
+};
+
+const enterEmployee = () => {
+    inquirer.prompt ({
+        name: "employee_first_name",
+        type: "input",
+        message: "Enter employee first name."
+    })
+    .then(data => {
+        db.query(`INSERT INTO employee (first_name) VALUES (?);`, data.first_name, (err) => {
+            if (err) {
+                throw err
+            } else {
+                console.log("First name added.");
+                init();
+            }
+        })
+    })
+};
+
+init();
